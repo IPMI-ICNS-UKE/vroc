4a2858593e8dbf3dc8bf1de4e103db5c4704aca2
Frederic Madesta <frederic.madesta@gmail.com>
2022-04-08 15:39:36 +0200

    cleanup and formatting

--- vroc/blocks.py
+++ vroc/blocks.py
@@ -4,7 +4,6 @@ from typing import Tuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Tuple
 
 
 class BaseGaussianSmoothing(ABC, nn.Module):
@@ -52,11 +51,11 @@ class BaseGaussianSmoothing(ABC, nn.Module):
         kernels = []
         for i in range(n_dim):
             if same_size:
-                kernel_1d = GaussianSmoothing3d._make_gaussian_kernel_1d(
+                kernel_1d = BaseGaussianSmoothing._make_gaussian_kernel_1d(
                     sigma=sigma[i], radius=max_kernel_radius
                 )
             else:
-                kernel_1d = GaussianSmoothing3d._make_gaussian_kernel_1d(
+                kernel_1d = BaseGaussianSmoothing._make_gaussian_kernel_1d(
                     sigma=sigma[i], sigma_cutoff=sigma_cutoff[i]
                 )
             if n_dim == 3:
@@ -95,8 +94,8 @@ class GaussianSmoothing2d(BaseGaussianSmoothing):
             kernel_y[None, None],
         )
         if self.same_size:
-            self.kernel = torch.cat((kernel_x, kernel_y), dim=0)
-            self.register_buffer("weight", self.kernel)
+            kernel = torch.cat((kernel_x, kernel_y), dim=0)
+            self.register_buffer("weight", kernel)
 
         else:
             self.register_buffer("weight_x", kernel_x)
@@ -172,12 +171,6 @@ class SpatialTransformer(nn.Module):
 
         # create sampling grid
         grid = self._create_grid(shape)
-
-        # registering the grid as a buffer cleanly moves it to the GPU, but it also
-        # adds it to the state dict. this is annoying since everything in the state dict
-        # is included when saving weights to disk, so the model files are way bigger
-        # than they need to be. so far, there does not appear to be an elegant solution.
-        # see: https://discuss.pytorch.org/t/how-to-register-buffer-without-polluting-state-dict
         self.register_buffer("grid", grid, persistent=False)
 
     def _create_grid(self, shape):
@@ -207,93 +200,54 @@ class SpatialTransformer(nn.Module):
             new_locs = new_locs.permute(0, 2, 3, 4, 1)
             new_locs = new_locs[..., [2, 1, 0]]
 
-        # s_t = time.time()
-        out = F.grid_sample(src, new_locs, align_corners=True, mode=self.mode)
-        # print(time.time()-s_t)
-        return out
+        return F.grid_sample(src, new_locs, align_corners=True, mode=self.mode)
 
 
 class DemonForces3d(nn.Module):
     @staticmethod
     def _calculate_demon_forces_3d(
-            image: torch.tensor,
-            reference_image: torch.tensor,
-            method: str,
-            original_image_spacing: Tuple[float, ...] = (1.0, 1.0, 1.0),
-            epsilon: float = 1e-6,
+        image: torch.tensor,
+        reference_image: torch.tensor,
+        method: str,
+        original_image_spacing: Tuple[float, ...] = (1.0, 1.0, 1.0),
+        epsilon: float = 1e-6,
     ):
         if method == "active":
             x_grad, y_grad, z_grad = torch.gradient(image, dim=(2, 3, 4))
         elif method == "passive":
             x_grad, y_grad, z_grad = torch.gradient(reference_image, dim=(2, 3, 4))
-            # TODO: Has to be only calculated once per level, as reference image does not change -> To be implemented
+            # TODO: Has to be only calculated once per level,
+            #  as reference image does not change -> To be implemented
         elif method == "symmetric":
             x_grad, y_grad, z_grad = torch.stack(
                 torch.gradient(image, dim=(2, 3, 4))
             ) + torch.stack(torch.gradient(reference_image, dim=(2, 3, 4)))
-            # TODO: Has to be only calculated once per level, as reference image does not change -> To be implemented
+            # TODO: Has to be only calculated once per level,
+            #  as reference image does not change -> To be implemented
         else:
-            raise Exception('Specified demon forces not implemented')
-        gamma = 1 / ((sum(i * i for i in original_image_spacing)) / len(original_image_spacing))
-        l2_grad = x_grad ** 2 + y_grad ** 2 + z_grad ** 2  # TODO: Same as above, if method == passive
+            raise Exception("Specified demon forces not implemented")
+        gamma = 1 / (
+            (sum(i * i for i in original_image_spacing)) / len(original_image_spacing)
+        )
+        l2_grad = (
+            x_grad**2 + y_grad**2 + z_grad**2
+        )  # TODO: Same as above, if method == passive
         norm = (reference_image - image) / (
             epsilon + l2_grad + gamma * (reference_image - image) ** 2
         )
 
         return norm * torch.cat((x_grad, y_grad, z_grad), dim=1)
 
-    def forward(self, image: torch.tensor, reference_image: torch.tensor, method: str,
-                original_image_spacing: Tuple[float, ...]):
+    def forward(
+        self,
+        image: torch.tensor,
+        reference_image: torch.tensor,
+        method: str,
+        original_image_spacing: Tuple[float, ...],
+    ):
         return DemonForces3d._calculate_demon_forces_3d(
-            image=image, reference_image=reference_image, method=method, original_image_spacing=original_image_spacing
+            image=image,
+            reference_image=reference_image,
+            method=method,
+            original_image_spacing=original_image_spacing,
         )
-
-
-if __name__ == "__main__":
-    # import numpy as np
-    # image = np.random.random((1, 3, 512, 512, 300)).astype(np.float32)
-    # image = torch.as_tensor(image, device='cuda')
-    #
-    # g1 = GaussianSmoothing3d(
-    #     sigma=(2, 2, 2),
-    #     sigma_cutoff=(2, 2, 2),
-    #     same_size=False
-    # ).to(image)
-    # g2 = GaussianSmoothing3d(
-    #     sigma=(2, 2, 2),
-    #     sigma_cutoff=(2, 2, 2),
-    #     same_size=True
-    # ).to(image)
-    #
-    # r1 = g1(image)
-    # r2 = g2(image)
-
-    import matplotlib.pyplot as plt
-    import time
-
-    image = torch.ones((1, 3, 10, 10, 10), device="cuda")
-    image[:, 1] *= 10
-
-    g1 = GaussianSmoothing3d(
-        sigma=(1, 1, 1), sigma_cutoff=(2, 2, 2), same_size=False
-    ).to(image)
-    g2 = GaussianSmoothing3d(
-        sigma=(1, 1, 1), sigma_cutoff=(2, 2, 2), same_size=True
-    ).to(image)
-
-    t = time.time()
-    r1 = g1(image)
-    print(time.time() - t)
-
-    t = time.time()
-    r2 = g2(image)
-    print(time.time() - t)
-
-    r1 = r1.detach().cpu().numpy().squeeze()
-    r2 = r2.detach().cpu().numpy().squeeze()
-
-    # fig, ax = plt.subplots(2, 2)
-    # ax[0, 0].imshow(r1[0])
-    # ax[0, 1].imshow(r1[1])
-    # ax[1, 0].imshow(r2[0])
-    # ax[1, 1].imshow(r2[1])
