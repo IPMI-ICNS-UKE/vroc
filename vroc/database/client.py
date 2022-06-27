7cc4fef1f98802c6657ce85c18b6de51e115926b
Frederic Madesta <frederic.madesta@gmail.com>
2022-06-27 09:27:19 +0200

    updated client: new fetching methods

--- vroc/database/client.py
+++ vroc/database/client.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import List, Optional, Union
+
+import pandas as pd
 
 import vroc.database.models as orm
 from vroc.common_types import PathLike
@@ -7,7 +9,8 @@ from vroc.logger import LoggerMixin
 
 class DatabaseClient(LoggerMixin):
     def __init__(self, database: PathLike):
-        orm.database.init(database=database)
+        self.database = orm.database
+        self.database.init(database=database)
         self._create_tables()
 
     def _create_tables(self):
@@ -34,3 +37,57 @@ class DatabaseClient(LoggerMixin):
             metric_after=metric_after,
             level_metrics=level_metrics,
         )
+
+    def fetch_runs(
+        self, image_id: Optional[str] = None, as_dataframe: bool = False
+    ) -> Union[List[dict], pd.DataFrame]:
+        query = orm.Run.select()
+        if image_id is not None:
+            query = query.where(orm.Run.image == image_id)
+
+        runs = list(query.dicts())
+
+        if as_dataframe:
+            runs = DatabaseClient._to_dataframe(runs)
+
+        return runs
+
+    def fetch_best_run(self, image_id: str) -> dict:
+        query = (
+            orm.Run.select()
+            .where(orm.Run.image == image_id)
+            .order_by(orm.Run.metric_after)
+            .limit(1)
+            .dicts()
+            .first()
+        )
+
+        return query
+
+    def fetch_best_runs(
+        self, as_dataframe: bool = False
+    ) -> Union[List[dict], pd.DataFrame]:
+        best_runs = []
+
+        for image in orm.Image.select():
+            best_runs.append(self.fetch_best_run(image))
+
+        if as_dataframe:
+            best_runs = DatabaseClient._to_dataframe(best_runs)
+
+        return best_runs
+
+    @staticmethod
+    def _expand_param_dict(run: dict):
+        run = run.copy()
+        params = run.pop("parameters")
+        for param_name, param_value in params.items():
+            run[param_name] = param_value
+
+        return run
+
+    @staticmethod
+    def _to_dataframe(runs: List[dict]) -> pd.DataFrame:
+        runs = [DatabaseClient._expand_param_dict(r) for r in runs]
+
+        return pd.DataFrame.from_records(runs, index="uuid")
