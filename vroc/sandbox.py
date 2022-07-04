6817cc7209469abeb1d907b0059bf001e54e21aa
thiloicns <t.sentker@uke.de>
2022-07-04 14:10:32 +0200

    TRE computation now solely implemented in numpy

--- vroc/sandbox.py
+++ vroc/sandbox.py
@@ -7,16 +7,8 @@ import numpy as np
 import SimpleITK as sitk
 import torch
 
-from vroc.helper import (
-    detach_and_squeeze,
-    landmark_distance,
-    load_and_preprocess,
-    read_landmarks,
-    scale_vf,
-    target_registration_errors,
-    target_registration_errors_snapped,
-    transform_landmarks_and_flip_z,
-)
+from vroc.dataset import BaseDataset
+from vroc.helper import compute_tre, read_landmarks
 from vroc.models import TrainableVarRegBlock
 
 matplotlib.use("module://backend_interagg")
@@ -29,17 +21,26 @@ n_level = 2
 data_path = f"/home/tsentker/data/dirlab2022/data/Case{dirlab_case:02d}Pack"
 out_path = "/home/tsentker/Documents/results/varreg-on-crack/"
 
-fixed = load_and_preprocess(os.path.join(data_path, "Images", "phase_0.mha"))
+fixed = BaseDataset.load_and_preprocess(
+    os.path.join(data_path, "Images", "phase_0.mha")
+)
 orig_ref = fixed
 original_image_spacing = fixed.GetSpacing()
-moving = load_and_preprocess(os.path.join(data_path, "Images", "phase_5.mha"))
+moving = BaseDataset.load_and_preprocess(
+    os.path.join(data_path, "Images", "phase_5.mha")
+)
 mask = sitk.ReadImage(os.path.join(data_path, "segmentation", f"mask_0.mha"))
 moving = sitk.HistogramMatching(
     moving, fixed, numberOfHistogramLevels=1024, numberOfMatchPoints=7
 )
+
+# load to numpy and swap back to x,y,z orientation
 fixed = sitk.GetArrayFromImage(fixed)
+fixed = np.swapaxes(fixed, 0, 2)
 moving = sitk.GetArrayFromImage(moving)
+moving = np.swapaxes(moving, 0, 2)
 mask = sitk.GetArrayFromImage(mask)
+mask = np.swapaxes(mask, 0, 2)
 
 patch_shape = fixed.shape
 fixed_ = torch.from_numpy(fixed.copy())
@@ -48,59 +49,45 @@ moving_ = torch.from_numpy(moving.copy())
 moving_ = moving_[None, None, :].float().to(device)
 mask_ = torch.from_numpy(mask.copy())
 mask_ = mask_[None, None, :].float().to(device)
-# mask_ = torch.ones_like(fixed_).float().to(device)
 
 model = TrainableVarRegBlock(
-    patch_shape=patch_shape,
-    iterations=(1100,) * n_level,
+    iterations=(800,) * n_level,
     demon_forces="symmetric",
-    tau=(6,) * n_level,
-    regularization_sigma=((3.0, 3.0, 3.0),) * n_level,
-    # radius=(4, 4, 4, 4),
+    tau=(2,) * n_level,
+    regularization_sigma=((2.0, 2.0, 2.0),) * n_level,
     scale_factors=(1 / 2, 1 / 1),
-    disable_correction=True,
-    early_stopping_method=("none", "lstsq"),
-    early_stopping_delta=(10, 1e-3),
-    early_stopping_window=(10, 20),
 ).to(device)
-_, warped, _, vf, metrics, _ = model.forward(
-    fixed_, mask_, moving_, original_image_spacing
-)
+warped, vf, _ = model.forward(fixed_, mask_, moving_, original_image_spacing)
 
-warped_moving = detach_and_squeeze(warped)
-warped_moving.CopyInformation(orig_ref)
-sitk.WriteImage(warped_moving, os.path.join(out_path, "warped_moving.mha"))
-
-vector_field = detach_and_squeeze(vf, is_vf=True)
-vector_field = scale_vf(vector_field, spacing=orig_ref.GetSpacing())
-vector_field.CopyInformation(orig_ref)
-
-sigma_jac = sitk.DisplacementFieldJacobianDeterminant(vector_field, True)
-sigma_jac = sitk.GetArrayFromImage(sigma_jac)[sitk.GetArrayFromImage(mask) == 1]
-
+# warped_moving = detach_and_squeeze(warped)
+# warped_moving.CopyInformation(orig_ref)
+# # sitk.WriteImage(warped_moving, os.path.join(out_path, "warped_moving.mha"))
+#
+# vector_field = detach_and_squeeze(vf, is_vf=True)
+# vector_field = scale_vf(vector_field, spacing=orig_ref.GetSpacing())
+# vector_field.CopyInformation(orig_ref)
+#
+# # sigma_jac = sitk.DisplacementFieldJacobianDeterminant(vector_field, True)
+# # sigma_jac = sitk.GetArrayFromImage(sigma_jac)[sitk.GetArrayFromImage(mask) == 1]
+#
 fixed_LM = read_landmarks(
     os.path.join(data_path, "extremePhases", f"Case{dirlab_case}_300_T00_xyz.txt")
 )
-fixed_LM_world = transform_landmarks_and_flip_z(fixed_LM, orig_ref)
 moving_LM = read_landmarks(
     os.path.join(data_path, "extremePhases", f"Case{dirlab_case}_300_T50_xyz.txt")
 )
-moving_LM_world = transform_landmarks_and_flip_z(moving_LM, orig_ref)
 
-delta_LM = landmark_distance(fixed_LM_world, moving_LM_world)
 
-vf_transformed = sitk.DisplacementFieldTransform(vector_field)
-TRE_world = target_registration_errors_snapped(
-    vf_transformed, fixed_LM_world, moving_LM_world, orig_ref, world=True
-)
-TRE_voxel = target_registration_errors_snapped(
-    vf_transformed, fixed_LM_world, moving_LM_world, orig_ref, world=False
-)
-TRE_world_no_snap = target_registration_errors(
-    vf_transformed, fixed_LM_world, moving_LM_world
-)
+# Fixes DIRLAB z orientation in landmarks by flipping last dimension
+fixed_LM = [(p[0], p[1], orig_ref.GetSize()[2] - p[2]) for p in fixed_LM]
+moving_LM = [(p[0], p[1], orig_ref.GetSize()[2] - p[2]) for p in moving_LM]
 
-print("| Case |  voxel TRE  | world TRE[mm] | no snap world TRE[mm] | sigma jacobian |")
-print(
-    f"|  {dirlab_case:02d}  | {np.mean(TRE_voxel):.2f} ({np.std(TRE_voxel):.2f}) |  {np.mean(TRE_world):.2f} ({np.std(TRE_world):.2f})  |      {np.mean(TRE_world_no_snap):.2f} ({np.std(TRE_world_no_snap):.2f})      |      {np.std(sigma_jac):.2f}      |"
+vf = vf.cpu().detach().numpy()
+vf = np.squeeze(vf)
+TRE_numpy = compute_tre(
+    fix_lms=np.array(fixed_LM),
+    mov_lms=np.array(moving_LM),
+    disp=vf,
+    spacing_mov=orig_ref.GetSpacing(),
+    snap_to_voxel=True,
 )
